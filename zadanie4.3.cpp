9ce0969eb8e386d7841bf5ca4f5edf5121bf497b
mtalarek <82826461+mtalarek@users.noreply.github.com>
2024-09-18 22:07:16 +0200

    Create zadanie4.3.cpp

---
+++ zadanie4.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+double oblicz_pierwiastek(double x, double e) {
+    int i = 0;
+    double w = 1;
+    while (abs((w * w) - x) >= e) {
+        w = ((x / w) + w) / 2;
+        i++;
+        cout << fixed << setprecision(8) << "wynik: " << w << ", iteracja: " << i << endl;
+        }
+    return w;
+}
+
+int main()
+{
+    double x, e;
+        cout << "podaj z jakiej liczby obliczyc pierwiastek oraz z jaka dokladnoscia: ";
+    cin >> x >> e;
+    // wynik = oblicz_pierwiastek(x, e);
+    cout << "pierwiastek drugiego stopnia z: " << defaultfloat << x << " to: " << fixed << setprecision(8) << oblicz_pierwiastek(x, e);
+}
